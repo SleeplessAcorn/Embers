468d47079782bf0cef23c07b1964545889c6efe0
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-08-03 20:42:14 -0700

    Future proof resource recipe condition

--- src/main/java/teamroots/embers/recipe/ConditionResourceEnabled.java
+++ src/main/java/teamroots/embers/recipe/ConditionResourceEnabled.java
@@ -4,8 +4,13 @@ import com.google.gson.JsonObject;
 import net.minecraft.util.JsonUtils;
 import net.minecraftforge.common.crafting.IConditionFactory;
 import net.minecraftforge.common.crafting.JsonContext;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import teamroots.embers.ConfigHandler;
+import teamroots.embers.Embers;
 
+import java.lang.reflect.Field;
+import java.util.Locale;
 import java.util.function.BooleanSupplier;
 
 public class ConditionResourceEnabled implements IConditionFactory {
@@ -13,14 +18,18 @@ public class ConditionResourceEnabled implements IConditionFactory {
     @Override
     public BooleanSupplier parse(JsonContext context, JsonObject json) {
         String type = JsonUtils.getString(json, "material");
-        switch (type) {
-            case "nickel": return () -> ConfigHandler.compat.enableNickel;
-            case "tin": return () -> ConfigHandler.compat.enableTin;
-            case "aluminum": return () -> ConfigHandler.compat.enableAluminum;
-            case "bronze": return () -> ConfigHandler.compat.enableBronze;
-            case "electrum": return () -> ConfigHandler.compat.enableElectrum;
+        try {
+            Field configField = FieldUtils.getField(ConfigHandler.ConfigCompat.class, "enable" + WordUtils.capitalize(type.toLowerCase(Locale.ROOT)));
+            return () -> {
+                try {
+                    return (Boolean) configField.get(ConfigHandler.compat);
+                } catch (Exception e) {
+                    return false;
+                }
+            };
+        } catch (Exception e) {
+            Embers.LOGGER.error("Error loading config value for material type {}", type);
+            return () -> false;
         }
-
-        return () -> false;
     }
 }
